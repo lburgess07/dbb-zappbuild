0b93afbea3f1be0c560ea5662d8cee7918571df4
Luke Burgess <luke.burgess@ibm.com>
2022-10-24 21:11:42 -0400

    format

--- test/test.groovy
+++ test/test.groovy
@@ -47,11 +47,11 @@ finally {
 	// Print test results
 	println("\nTest Results:")
 	// Set up formatting
-	String leftAlignFormat = "%-4d | %-25s | %-25s |%n";
+	String leftAlignFormat = "%-4d | %-25s | %-25s %n";
 	// Print header
-	//System.out.format("+------+---------------------------+-----------------------------------------------------------------------------+%n");
-	System.out.format("Num  | Test Name                 | Result %n");
-	//System.out.format("+------+---------------------------+-----------------------------------------------------------------------------+%n");
+	System.out.format("-----+---------------------------+------------%n");
+	System.out.format("Num  | Test Name                 | Result     %n");
+	System.out.format("-----+---------------------------+------------%n");
 	// Print data
 	testList.eachWithIndex { testName, index -> 
 		def testNum = index + 1
@@ -60,7 +60,7 @@ finally {
 			testResult = testResult.substring(0, testResult.indexOf("Expression")) // Strip 'Expression: *' part of message out
 		System.out.format(leftAlignFormat, testNum, testName, testResult)
 	}
-	//System.out.format("+------+---------------------------+-----------------------------------------------------------------------------+%n");
+	System.out.format("-----+---------------------------+------------%n");
 }
 // end script
 
