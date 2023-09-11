86b413e2eb73935d72a079fa129a34496c59478b
Luke Burgess <luke.burgess@ibm.com>
2023-09-11 10:18:53 -0400

    try array command exec

--- utilities/GitUtilities.groovy
+++ utilities/GitUtilities.groovy
@@ -114,7 +114,12 @@ def getRemoteGitBranches(String gitDir) {
  * @param  String gitDir  		Local Git repository directory
  */
 def isGitDetachedHEAD(String gitDir) {
-	String cmd = "git -C \"$gitDir\" status"
+	String[] cmd 
+	cmd << "git"
+	cmd << "-C"
+	cmd << gitDir
+	cmd << "status"
+	//String cmd = "git -C \"$gitDir\" status"
 	StringBuffer gitStatus = new StringBuffer()
 	StringBuffer gitError = new StringBuffer()
 
