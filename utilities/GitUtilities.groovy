ffd187caaedaf6a33fda97b503ca0f920b788209
Luke Burgess <luke.burgess@ibm.com>
2023-09-27 13:25:51 -0400

    remove debug stmnts

--- utilities/GitUtilities.groovy
+++ utilities/GitUtilities.groovy
@@ -357,9 +357,7 @@ def getChangedFiles(List<String> cmd) {
 	for (line in git_diff.toString().split("\n")) {
 		// process files from git diff
 		try {
-			println("Line: " + line)
 			gitDiffOutput = line.split('\\t') // split output by tab (delimiter used by git)
-			println("Line split: " + gitDiffOutput)
 			action = gitDiffOutput[0]
 			file = gitDiffOutput[1]
 
